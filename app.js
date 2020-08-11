0b0f70f942efeebff3912d91692c0296aedd39e3
usukhbayar otgonbat <capusukh16@gmail.com>
2020-08-11 19:51:58 +0800

    first change

---
+++ app.js
@@ -0,0 +1,34 @@
+// DOM's
+
+const hiih = document.getElementById("hiih");
+const lockerOne = document.getElementById("one");
+const lockerTwo = document.getElementById("two");
+const lockerThree = document.getElementById("three");
+const lockerFour = document.getElementById("four");
+const lockerFive = document.getElementById("five");
+const lockerSix = document.getElementById("six");
+const lockerSeven = document.getElementById("seven");
+const lockerEight = document.getElementById("eight");
+const lockerNine = document.getElementById("nine");
+const lockerTen = document.getElementById("ten");
+const lockerEleven = document.getElementById("eleven");
+const lockerTwelve = document.getElementById("twelve");
+const lockerThirteen = document.getElementById("thirteen");
+const lockerFourteen = document.getElementById("fourteen");
+const lockerFifteen = document.getElementById("fifteen");
+const lockerSixteen = document.getElementById("sixteen");
+const lockerSeventeen = document.getElementById("seventeen");
+const lockerEighteen = document.getElementById("eighteen");
+const lockerNineteen = document.getElementById("nineteen");
+const lockerTwenty = document.getElementById("twenty");
+
+const teeshHiih = document.getElementById("hiih");
+const teeshAvah = document.getElementById("avah");
+
+teeshHiih.addEventListener("click", function () {
+  window.location.href = "password.html";
+});
+
+teeshAvah.addEventListener("click", function () {
+  window.location.href = "password.html";
+});
